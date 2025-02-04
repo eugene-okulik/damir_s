e573209574b8e2f63df42a4c413bc8ce0a8803b1
damir_s <damir672@gmail.com>
2025-02-05 00:09:03 +0300

    lesson_3.3

--- homework/damir_s/homework_3/Task_3.py
+++
@@ -1,5 +0,0 @@
-import math
-x=10 
-y=2
-print((x+y)/2)
-print(math.sqrt(x*y))
--- homework/damir_s/homework_3/Task_4.py
+++
@@ -1,8 +0,0 @@
-import math
-a=3
-b=4
-c = math.sqrt(a**2+b**2)
-print("Гипотенуза",c)
-
-S=(a*b)/2
-print("Площадь",S)
